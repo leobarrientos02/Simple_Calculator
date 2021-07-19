a55c9620a92838ac68de71c56d0a7bd3718d5340
Your Name <leobarrientos02@gmail.com>
2021-07-19 00:06:50 -0400

    getDisplayNumber function
    This function allows for the display to show the numbers in a mathematical format

--- app.js
+++ app.js
@@ -66,12 +66,38 @@ class Calculator {
     this.previousOperand = ``;
   }
 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = ``;
+    } else {
+      integerDisplay = integerDigits.toLocaleString(`en`, {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   updateDisplay() {
     // Set the current Operand display
-    this.currentOperandTextElement.innerText = this.currentOperand;
-
-    // Set the previous Operand display
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      // Set the previous Operand display
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = ``;
+    }
   }
 }
 // Declare Variables
